db066c73f52757e270a6193055d520088638de71
ryanrare <ryan.hollmes@gmail.com>
2024-02-21 21:43:16 -0300

    reutilizando css atraves de componentes

--- cookin-up/src/components/CardCategoria.vue
+++ cookin-up/src/components/CardCategoria.vue
@@ -1,11 +1,13 @@
 <script lang="ts">
 import type ICategoria from '@/interfaces/ICategorias';
 import type { PropType } from 'vue';
+import Tag from './Tag.vue';
 
 export default {
     props: {
-        categoria: {  type: Object as PropType<ICategoria>, required: true  }
-    }
+        categoria: { type: Object as PropType<ICategoria>, required: true }
+    },
+    components: { Tag }
 }
 </script>
 
@@ -19,7 +21,7 @@ export default {
 
         <ul class="categoria__ingredientes">
             <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
-                {{ ingrediente }}
+                <Tag :texto="ingrediente" />
             </li>
         </ul>
     </article>
